dbb645d34e853c7c72a528e4288419f8c36e8271
Viktor Myhrberg <viktor.myhrberg@hotmail.se>
2018-10-10 12:29:41 +0200

    Laptop setup
    Hoppas det fungerar.... UNSTABLE

--- Project/Project/main.cpp
+++ Project/Project/main.cpp
@@ -1,7 +1,9 @@
 #include "SDL.h"
+#include "Vector2.h"
 
 int main(int argc, char *argv[])
 {
+	Vector2 heyyy;
 	{
 		SDL_Init(SDL_INIT_EVERYTHING);
 		SDL_Window *window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
